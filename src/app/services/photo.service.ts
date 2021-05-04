7954f4fb787433cf3ecfee3f74981a16388f0def
ngui27 <guilhemlaieb@gmx.fr>
2021-05-04 16:54:35 +0200

    initiation a ionic

---
+++ src/app/services/photo.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+
+// import Capacitor dependencies and get references to the Camera, Filesystem, and Storage plugins
+import { Plugins, CameraResultType, Capacitor, FilesystemDirectory, 
+  CameraPhoto, CameraSource } from '@capacitor/core';
+
+const { Camera, Filesystem, Storage } = Plugins;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PhotoService {
+
+  constructor() { }
+  public async addNewToGallery() {
+    // Take a photo
+    const capturedPhoto = await Camera.getPhoto({
+      resultType: CameraResultType.Uri, 
+      source: CameraSource.Camera, 
+      quality: 100 
+    });
+  }
+}
